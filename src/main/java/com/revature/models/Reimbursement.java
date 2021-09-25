7253eea99d560d5bdb1389c05a670b5ac66301a1
ptrcktylr <ptrcktylr@icloud.com>
2021-09-25 12:03:37 -0400

    added .gitignored files, removed gradle default files

---
+++ src/main/java/com/revature/models/Reimbursement.java
@@ -0,0 +1,9 @@
+package com.revature.models;
+
+public class Reimbursement {
+	private int id;
+	private double amount;
+	private String submitted_at;
+	private String resolved_at;
+	private String description;
+}
